b42e21dbb4f415ef4e2b823209ac4cea1902dbd7
Hongxu Wang <hongxuwa@Hongxus-MacBook-Pro.local>
2024-10-09 16:11:48 -0700

    adding examples

--- R/plot_lafamum.R
+++ R/plot_lafamum.R
@@ -25,6 +25,32 @@
 #'
 #' @export
 #'
+#' @examples
+#'  # Load Example Data
+#'  data("simulated_data")
+#'  covars <- c("e3_sex_None", "hs_child_age_yrs_None")
+#'
+#'  # Extract exposure and outcome data
+#'  exposure <- simulated_data[["phenotype"]]$hs_hg_m_scaled
+#'  outcome  <- simulated_data[["phenotype"]]$ck18_scaled
+#'
+#'  # Get numeric matrix of covariates
+#'  covs <- simulated_data[["phenotype"]][covars]
+#'  covs$e3_sex_None <- ifelse(covs$e3_sex_None == "male", 1, 0)
+#'
+#'  # create list of omics data
+#'  omics_lst <- simulated_data[-which(names(simulated_data) == "phenotype")]
+#'
+#'  # Latent Factor Multiomic Mediation with early integration
+#'  result_lafamum_early <- lafamum(exposure = exposure,
+#'                                  outcome  = outcome,
+#'                                  omics_lst = omics_lst,
+#'                                  covs = covs,
+#'                                  Y.family = "gaussian",
+#'                                  integration = "early")
+#'  # Plot result
+#'  fig <- plot_lafamum(result_lafamum_early)
+#'
 plot_lafamum <- function(lafamum_res,
                          n_features = 10,
                          plot_type = "all") {
